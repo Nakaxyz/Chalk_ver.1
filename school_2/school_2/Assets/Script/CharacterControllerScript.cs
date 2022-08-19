6af4d86becd4418f74ab4cb1e31b57c9ce424f75
Nakaxyz <kirisamereima@gmail.com>
2022-08-20 02:54:10 +0900

    xyz1

--- school_2/school_2/Assets/Script/CharacterControllerScript.cs
+++ school_2/school_2/Assets/Script/CharacterControllerScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
  
 public class CharacterControllerScript : MonoBehaviour
 {
-    private float speed;
+    public float speed;
 
     void Start()
     {
---
+++ school_2/school_2/Assets/Script/TimeControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+        public GameObject PlayerControl,Option;
+        
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Option.activeSelf)
+		{
+            PlayerControl.GetComponent<CharacterControllerScript>().speed = 0;
+        }
+		else
+		{
+            PlayerControl.GetComponent<CharacterControllerScript>().speed = 1;
+        }
+        Debug.Log(Option.activeSelf);
+            Debug.Log(PlayerControl.GetComponent<CharacterControllerScript>().speed);
+    
+    }
+}
