ece1c48dc21a0de754808c0965560b99a18f2fae
daiki1357 <86827306+daiki1357@users.noreply.github.com>
2022-10-22 17:03:57 +0900

    Revert "10/07"
    This reverts commit 54776f7bfdbb46d344c526fee5ad7ee308ebc144.

--- school_2 2/Assets/script/OptionSetting.cs
+++ school_2 2/Assets/script/OptionSetting.cs
@@ -15,27 +15,28 @@ public class OptionSetting :MonoBehaviour
 
     }
 
-		  void Update()
-		{
-			if (Input.GetKeyDown(KeyCode.Escape))
-			{
-				Debug.Log("‰Ÿ‚µ‚Ä‚é‚æ");
+    // Update is called once per frame
+  //  void Update()
+  //  {
+		//if (Input.GetKeyDown(KeyCode.Escape))
+		//{
+  //          Debug.Log("‰Ÿ‚µ‚Ä‚é‚æ");
+            
+  //          if (Option.activeSelf==true)
+  //          {
+  //              Option.SetActive (false);
+  //              Debug.Log("Active" + Option.activeSelf); 
+  //          }
+            
+  //          else//(Option.activeSelf == false)
+  //          {
+  //              Option.SetActive(true);
+  //              Debug.Log("Active" + Option.activeSelf);
+  //          }
+  //      }
+  //  }
 
-				if (Option.activeSelf == true)
-				{
-					Option.SetActive(false);
-					Debug.Log("Active" + Option.activeSelf);
-				}
-
-				else//(Option.activeSelf == false)
-				{
-					Option.SetActive(true);
-					Debug.Log("Active" + Option.activeSelf);
-				}
-			}
-		}
-
-		public void _OptionOpen()
+        public void _OptionOpen()
 		{
             Option.SetActive(true);
             Debug.Log("Active" + Option.activeSelf);
